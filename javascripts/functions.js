eb50d798471f4a57b6722a6811fcb3fe4565f449
Sebastian Shanafelt <sshanafe@uoregon.edu>
2015-04-17 15:44:49 -0700

    Add Javascript

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -46,5 +46,38 @@ var allArmstrongs = function() {
 //requirement C
 
 var allSubstrings1 = function(s) {
-	for (var i = 0;)
+	var n = s.length;
+	result = "";
+	for(i = 0; i < n; i++){
+		for(k = i; k < n; k++){
+			result += s.substring(i, k + 1) + ", ";
+		}
+	}
+	return result.substring(0, result.length - 2);
+}
+
+//requirement D
+
+var allSubstrings2 = function(s) {
+	var n = s.length;
+	result = [];
+	for(i = 0; i < n; i++){
+		for(k = i; k < n; k++){
+			result.push(s.substring(i, k + 1));
+		}
+	}
+	return result;
+}
+
+//requirement E
+
+var maxWord = function(s) {
+	var maxWord = "";
+	var stringArray = s.split(" ");
+	for(i = 0; i < stringArray.length; i++){
+		if(stringArray[i].length > maxWord.length){
+			maxWord = stringArray[i];
+		}
+	}
+	return maxWord;
 }
